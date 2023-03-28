09892ca0288c2496f9e011d0b691af8f8e58085f
SathyajeethPatil <128781686+SathyajeethPatil@users.noreply.github.com>
2023-03-28 17:40:56 +0530

    Add files via upload

---
+++ Lecture_3.java
@@ -0,0 +1,29 @@
+package Lecture;
+
+public class Lecture_3 {
+
+	public static void main(String[] args) {
+		
+		int aInt = 42;
+		short aShort = 97;
+		long aLong = 8304;
+		double aDouble = 3.542;
+		float aFloat = 5.423f;
+		char aChar = 'b';
+		boolean aBoolean = true;
+		byte aByte = 127;
+				
+				
+		System.out.println(aInt);
+		System.out.println(aShort);
+		System.out.println(aLong);
+		System.out.println(aDouble);
+		System.out.println(aFloat);
+		System.out.println(aChar);
+		System.out.println(aBoolean);
+		System.out.println(aByte);
+		
+
+	}
+
+}
---
+++ Lecture_4.java
@@ -0,0 +1,21 @@
+package Lecture;
+
+public class Lecture_4 {
+
+	public static void main(String[] args) {
+		// String Lecture
+		int aNumber = 9;
+		double aDouble = 9.5;
+		String text = "Sathya";
+		String blank = " ";
+		String moreText = "here";
+		String hiHere = text + blank + moreText;
+		
+		System.out.println(text);
+		System.out.println(text + blank + moreText);
+		System.out.println("Hi" + " " + "here");
+		System.out.println(hiHere);
+		System.out.println("My Number:" + aNumber + ", " + aDouble);
+	}
+
+}
